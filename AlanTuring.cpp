44a7171b8031a08d07f8011d87b352b12a378c37
Raksamon Kumpaphan <hutchiw@users.noreply.github.com>
2018-01-13 11:44:32 +0700

    C++
    It's hard to do,but it doesn't mean that do not.

---
+++ AlanTuring.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+using namespace std ;
+
+int main(){
+	string name ;
+	cout << "    Programming is a skill best acquired\n";
+	cout << "by prayice and example rather than from book.\n" ;
+	cout << "\n               #Alan Turing" ;
+	
+	return 0 ;
+}
+
